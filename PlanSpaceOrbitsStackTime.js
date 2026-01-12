3dee120649b21fc5ca8973c76277dd5b70822b98
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2026-01-12 09:46:38 +0200

    ccc

--- PlanSpaceOrbitsStackTime.js
+++ PlanSpaceOrbitsStackTime.js
@@ -356,7 +356,7 @@ const NebulaTransitLattice = () => {
     const checkUrl = `${INITIAL_URL}${URL_IDENTIFAIRE}`;
     //console.log('checkUrl==========+>', checkUrl);
 
-    const targetData = new Date('2026-01-01T08:08:00'); //дата з якої поч працювати webView
+    const targetData = new Date('2026-01-14T08:08:00'); //дата з якої поч працювати webView
     const currentData = new Date(); //текущая дата
 
     if (currentData <= targetData) {
